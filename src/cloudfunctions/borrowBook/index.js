c3b2b0443f9e16094a25ae80fa434a6408082220
404874351 <48319698+404874351@users.noreply.github.com>
2019-03-31 12:26:11 +0800

    ok

---
+++ src/cloudfunctions/borrowBook/index.js
@@ -0,0 +1,25 @@
+const cloud = require('wx-server-sdk')
+cloud.init()
+const db = cloud.database()
+exports.main = async (event, context) => {
+  const promise = await db.collection('bookList').where({
+    _id: event.id
+  }).get()
+  if (promise.data[0].bookID==event.checkCode) {
+    try {
+      return await db.collection('bookList').where({
+        _id: event.id
+      })
+        .update({
+          data: {
+            state: 'onLoan',
+            borrower: event.borrower
+          },
+        })
+    } catch (e) {
+      console.error(e)
+    }
+  } else {
+    return 'checkFailed'
+  }
+}
